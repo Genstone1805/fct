de88e5d08e01b53ef11496a1aed98c770042c915
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-01-27 05:56:57 +1400

    Bug Fixes

--- driver/views.py
+++ driver/views.py
@@ -117,19 +117,18 @@ class CreateDriverView(APIView):
             )
                 
         except ValidationError as e:
+            print(serializer.errors)
             return Response(
                 {'error': 'Validation error', 'details': e.detail},
                 status=status.HTTP_400_BAD_REQUEST
             )
             
         except Exception as e:
+            print(serializer.errors)
             return Response(
                 {'error': 'Failed to update route', 'details': str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
-
-
-
 
 class DriverListView(generics.ListAPIView):
     """List all users where is_driver=True"""
