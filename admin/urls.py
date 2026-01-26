a3251beffd1f4ad2e1071f30d0c046f48a685833
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-01-26 14:48:42 +1400

    bug fixes

--- admin/urls.py
+++ admin/urls.py
@@ -8,5 +8,5 @@ from .views import (
 urlpatterns = [
     path('routes/', RouteListView.as_view(), name='admin-route-list'),
     path('routes/create/', CreateRouteView.as_view(), name='admin-route-create'),
-    path('routes/<str:route_id>/detail/', RetrieveUpdateDestroyRouteView.as_view(), name='admin-route-update'),
+    path('routes/<str:route_id>', RetrieveUpdateDestroyRouteView.as_view(), name='admin-route-update'),
 ]
