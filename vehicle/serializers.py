5bfb023be8d32f0eab204dfdcf1088b189097b32
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-01-27 23:42:17 +1400

    Bug fixes

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -5,7 +5,7 @@ from .models import Vehicle
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
-        fields = ['license_plate', 'make', 'model', 'year', 'color', 'type', "max_passengers"]
+        fields = ['id','license_plate', 'make', 'model', 'year', 'color', 'type', "max_passengers"]
         read_only_fields = ['id', 'created_at', 'updated_at']
 
 class AvailableVehicleSerializer(serializers.ModelSerializer):
