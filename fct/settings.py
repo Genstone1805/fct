e7880524cf0fa120009c1fa204b8a1fcb9409f16
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-02-04 03:13:28 +1400

    bug fixes

--- fct/settings.py
+++ fct/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 API_KEY = config('API_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "firstclasstransfers.eu", 
