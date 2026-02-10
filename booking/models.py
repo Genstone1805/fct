9fb497871917eea7d31b325106f9d1a66cb15def
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-02-11 02:11:14 +1400

    bug fixes

--- booking/models.py
+++ booking/models.py
@@ -78,7 +78,7 @@ class  Booking(models.Model):
   vehicle_type = models.CharField(choices=VEHICLE_TYPE, max_length=100)
   payment_type = models.CharField(choices=PAYMENT_TYPES, max_length=15 )
   payment_status = models.CharField(choices=PAYMENT_STATUS, max_length=15, default="pending")
-  transaction_id = models.CharField(max_length=100, null=True, blank=True)
+  transaction_id = models.CharField(max_length=100, default="")
   trip_type = models.CharField(choices=TRIP_TYPE_CHOICES, max_length=15 )
   pickup_date = models.DateField()
   pickup_time = models.TimeField()
